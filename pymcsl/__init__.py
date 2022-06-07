562aea52ea8d86c98b95c699c7d8c000273b894e
Filipe Chagas <filipe.ferraz0@gmail.com>
2022-06-07 01:34:20 -0400

    Update __init__.py

--- pymcsl/__init__.py
+++ pymcsl/__init__.py
@@ -5,4 +5,5 @@ sys.path.append(os.path.abspath(os.path.dirname(__file__)))
 __version__ = '0.1.0'
 from subsimulation import SubSimulationEnv, ContextType
 from montecarlosimulation import MonteCarloSimulationEnv
+from randomvariable import DiscreteRandomVariable
 from markovchain import SimpleMarkovChain
