ff27253bc157a71ce63cc10b93f58062028f33d1
Filipe Chagas <filipe.ferraz0@gmail.com>
2022-06-06 21:29:05 -0400

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -38,6 +38,8 @@ extensions = [
     'sphinx.ext.intersphinx',
 ]
 
+autoclass_content = 'both'
+
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
